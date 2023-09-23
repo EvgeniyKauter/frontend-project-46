2ea0d82c955219cdd8c3c0384dd6dd3b004a2df9
EvgeniyKauter <evgeniykauter@mail.ru>
2023-09-23 09:58:23 +0300

    update gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,5 +6,8 @@ program
   .name('gendiff')
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
 
 program.parse();
